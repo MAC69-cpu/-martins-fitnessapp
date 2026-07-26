280522ecba8954df26d1a9422764922686f931cf
MAC69-cpu <info@craul.de>
2026-07-26 16:42:40 +0200

    service-worker.js aktualisieren

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,5 @@
-
-const CACHE = "martins-fitnessapp-v2";
+const CACHE = "martins-fitnessapp-v3";
 const FILES = ["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
-self.addEventListener("install", e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))));
-self.addEventListener("activate", e => e.waitUntil(self.clients.claim()));
+self.addEventListener("install", e => { self.skipWaiting(); e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))); });
+self.addEventListener("activate", e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
 self.addEventListener("fetch", e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
